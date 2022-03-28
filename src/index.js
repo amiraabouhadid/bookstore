2b8f2d2b78d96ec9e3c4f204175c2a0540c2a6b9
amira <amiraabouhadid@gmail.com>
2022-03-28 17:53:39 +0200

    add reducers and actions using duck pattern

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import rootReducer from './redux/configureStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
