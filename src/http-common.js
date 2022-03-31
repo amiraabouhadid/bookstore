50ca46f33b2b36f6eb38bb6f2a568b63d76e7968
Amira <56790126+amiraabouhadid@users.noreply.github.com>
2022-03-31 12:27:49 +0200

    Revert "Code Review Bookstore: connect to API"

--- src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const appId = '7fNT9oRcLFhonCEOlR1G';
-export default axios.create({
-  baseURL: `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}`,
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
